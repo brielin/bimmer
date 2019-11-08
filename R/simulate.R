e10e7ab81bd0211f5ef13c310a43bbc73bfb0d3a
brielin <bb2991@columbia.edu>
2019-11-08 08:56:12 -0500

    Changes simulate.R and associated tests in preparation for larger-scale simulations to come.
    Changes include:
    - Adding sd param to network and effect size generation.
    - Adding symmetric/anti-symmetric network simulation.
    - Adds whitening option to X generation.
    - Removes diagonal beta hack.
    - Adds optional normalization to sumstats generation.
    - Adds oracle SNP selection method.

--- R/simulate.R
+++ R/simulate.R
@@ -8,15 +8,16 @@
 #' @param M Integer. Number of SNPs to simulate.
 #' @param D Integer. Number of phenotypes to simulate.
 #' @param p Float. Proportion of non-zero effects.
+#' @param sd Float. Standard deviation normal distribution of effect sizes.
 #' @param pleiotropy Bool. TRUE to allow for SNPs to effect multiple phenotypes.
 #' @return A list,
 #'   beta: sparse float matrix of dimension MxD.
 #'   condition: float vector of length D.
 #' @example generate_beta(100, 10, 0.2)
-generate_beta <- function(M, D, p = 0.1, pleiotropy = FALSE) {
+generate_beta <- function(M, D, p = 0.1, sd = 1.0, pleiotropy = FALSE) {
   # TODO(brielin): Separate beta generation from condition analysis?
   beta <- Matrix::Matrix(
-    stats::rnorm(M * D) * (stats::runif(M * D) < p), M, D,
+    stats::rnorm(M * D, sd = sd) * (stats::runif(M * D) < p), M, D,
     sparse = TRUE
   )
   colnames(beta) <- paste0("P", 1:D)
@@ -49,11 +50,28 @@ generate_beta <- function(M, D, p = 0.1, pleiotropy = FALSE) {
 #'   eigenvalues with modulus between -1 and 1.
 #' @param epsilon Float. Number to add to maximum eigenvalue to better-condition
 #'   normalization.
+#' @param symmetric Bool. TRUE to force the returned matrix to be
+#'   symmetric, false for anti-symmetric, and NULL for no symmetry enforcement.
 #' @return A DxD sparse matrix.
 #' @example generate_network(10, 0.2)
-generate_network <- function(D, p = 0.1, normalize = TRUE, epsilon = 0.1) {
-  R <- matrix(stats::rnorm(D * D) * (stats::runif(D * D) < p), D, D)
+generate_network <- function(D, p = 0.1, normalize = TRUE, epsilon = 0.1,
+                             symmetric = NULL, sd = 1.0) {
+  R <- matrix(stats::rnorm(D * D, sd = sd) * (stats::runif(D * D) < p), D, D)
   diag(R) <- 0.0
+  if (!is.null(symmetric)) {
+    if (symmetric) {
+      R[lower.tri(R)] <- 0
+      R <- R + t(R)
+    }
+    else if (!symmetric) {
+      # TODO(brielin): Fix this so that it evenly sets upper and lower indices
+      #   to 0 and preserves fraction of non-zeros.
+      mutual_ut <- which(
+        (abs(R)[upper.tri(R)] > 0) & (t(abs(R))[upper.tri(R)] > 0)
+      )
+      R[upper.tri(R)][mutual_ut] <- 0
+    }
+  }
   R <- Matrix::Matrix(R, sparse = TRUE)
   colnames(R) <- paste0("P", 1:D)
   rownames(R) <- paste0("P", 1:D)
@@ -64,22 +82,27 @@ generate_network <- function(D, p = 0.1, normalize = TRUE, epsilon = 0.1) {
       R <- R / (max_value + epsilon)
     }
   }
-  R
+  return(R)
 }
 
 #' Generates genotypes.
 #'
 #' @param N Integer. Number of individuals to simulate.
 #' @param M Integer. Number of SNPs to simulate.
+#' @param whiten Bool. TRUE to whiten such that the observed covariance is
+#'   exactly 0. This should only be used for testing.
 #' @return and NxM matrix of simulated genotypes.
 #' @example generate_genotype(100, 10)
-generate_genotypes <- function(N, M) {
+generate_genotypes <- function(N, M, whiten = FALSE) {
   # TODO(brielin): Get real genotypes from Biobank.
   X <- matrix(stats::rnorm(N * M), N, M)
-  # vars <- apply(X, 2, stats::var)
+  if (whiten) {
+    X <- t(t(X) - colMeans(X))
+    X_svd <- svd(X)
+    X <- sqrt(N - 1) * X_svd$u %*% t(X_svd$v)
+  }
   rownames(X) <- paste0("N", 1:N)
   colnames(X) <- paste0("rs", 1:M)
-  # t(t(X)/sqrt(vars))
   return(X)
 }
 
@@ -96,15 +119,21 @@ generate_genotypes <- function(N, M) {
 #' @param noise Float. Proprotion of variance in phenotype attributable to
 #'   noise. 0 for no noise, 1 for all noise with no genetic/network component.
 #' @param pleiotropy Bool. TRUE to allow for SNPs to effect multiple phenotypes.
+#' @param whiten Bool. TRUE to whiten generated genotypes. See
+#'   generate_genotypes for more information.
+#' @param symmetric Bool or NULL. Whether to enforce a symmetry constraint on
+#'   generated network. See generate_network for more.
+#' @return A list:
+#'   Y: N x D matrix of observed phenotypes.
+#'   X: N x M matrix of genotypes.
+#'   beta: M x D matrix of genotype effect sizes.
+#'   R: D x D network effect matrix.
 generate_dataset <- function(N, M, D, p_beta = 0.2, p_net = 0.2, noise = 0.0,
-                             pleiotropy = FALSE) {
-  X <- generate_genotypes(N, M)
-  if ((M == D) && pleiotropy == FALSE) {
-    beta_res <- list("beta" = diag(D), "condition" = rep(1e5, D))
-  } else {
-    beta_res <- generate_beta(M, D, p_beta, pleiotropy)
-  }
-  R <- generate_network(D, p_net)
+                             pleiotropy = FALSE, whiten = FALSE,
+                             symmetric = NULL) {
+  X <- generate_genotypes(N, M, whiten = whiten)
+  beta_res <- generate_beta(M, D, p_beta, pleiotropy)
+  R <- generate_network(D, p_net, symmetric = symmetric)
   network <- X %*% beta_res$beta %*% solve(diag(D) - R)
   network_var <- apply(network, 2, stats::var)
   epsilon <- t(t(matrix(stats::rnorm(N * D), N, D)) * sqrt(network_var))
@@ -116,18 +145,30 @@ generate_dataset <- function(N, M, D, p_beta = 0.2, p_net = 0.2, noise = 0.0,
 #'
 #' @param X N x M matrix of genotypes.
 #' @param Y N x D matrix of phenotypes.
+#' @param normalize Bool. If TRUE, normalize X and Y matrices to have variance
+#'   1.0. That is, return r_hat instead of beta_hat. Default TRUE.
 #' @return A list,
 #'   beta_hat: An M x D matrix of calculated effect sizes.
 #'   se_hat: An M x D matrix of corresponding estimated standard errors.
 #'   p_value: An M x D matrix of corresponding p-values.
 #'   r_squared: An M x D matrix of variances explained.
-generate_sumstats <- function(X, Y) {
+#'   n_mat: An M x D matrix of sample sizes. For simulations, this is just N.
+generate_sumstats <- function(X, Y, normalize = TRUE) {
   # TODO(brielin): Add test for this function.
   N <- dim(X)[1]
   M <- dim(X)[2]
   D <- dim(Y)[2]
   X_no_mean <- t(t(X) - colMeans(X))
   Y_no_mean <- t(t(Y) - colMeans(Y))
+  X_sd <- apply(X, 2, stats::sd)
+  Y_sd <- apply(Y, 2, stats::sd)
+  normalizer <- outer(X_sd, 1 / Y_sd)
+  if (normalize) {
+    X_no_mean <- t(t(X_no_mean) / X_sd)
+    Y_no_mean <- t(t(Y_no_mean) / Y_sd)
+    normalizer <- matrix(1L, M, D)
+  }
+
   diag_xtxi <- 1 / colSums(X_no_mean^2)
   beta_hat <- (diag_xtxi * t(X_no_mean)) %*% Y_no_mean
   calc_s <- function(X_col, beta_row) {
@@ -135,18 +176,41 @@ generate_sumstats <- function(X, Y) {
     X2sum <- sum(X_col^2)
     sqrt(eps2sum / ((N - 2) * X2sum))
   }
-  se_hat <- t(mapply(calc_s, data.frame(X), data.frame(t(beta_hat))))
+  se_hat <- t(mapply(calc_s, data.frame(X_no_mean), data.frame(t(beta_hat))))
   Z_hat <- beta_hat / se_hat
   p_value <- 2 * (1 - stats::pnorm(abs(Z_hat)))
 
-  Xvar <- apply(X, 2, stats::var)
-  Yvar <- apply(Y, 2, stats::var)
-  normalizer <- outer(sqrt(Xvar), sqrt(1 / Yvar))
+  n_mat <- matrix(rep(N, M * D), M, D)
+  rownames(n_mat) <- colnames(X)
+  colnames(n_mat) <- colnames(Y)
   r_squared <- (normalizer * beta_hat)^2
   list(
     "beta_hat" = beta_hat,
     "se_hat" = se_hat,
     "p_value" = p_value,
-    "r_squared" = r_squared
+    "r_squared" = r_squared,
+    "n_mat" = n_mat
+  )
+}
+
+#' Selects SNPs based on true effect sizes.
+#'
+#' @param beta M x D matrix of true effect sizes.
+#' @param p_val M x D matrix of observed p-values.
+#' @param p_thresh Float. Threshold for inclusion of a SNP.
+#' @return A list of lists. The outer list is indexed by the phenotype names.
+#'   Each inner list is also indexed by the phenotype names, and the value of
+#'   result$P1$P2 is a boolean vector of length M corresponding to the SNPs to
+#'   use in the estimation of TCE of P1 on P2.
+select_snps_oracle <- function(beta, p_val, p_thresh = 1e-5) {
+  pheno_names <- colnames(beta)
+  select <- function(beta_exp, p_val_exp) {
+    index <- (abs(beta_exp) > 0) & (p_val_exp < p_thresh)
+    sapply(pheno_names, function(x) {
+      return(index)
+    }, simplify = FALSE)
+  }
+  mapply(select, data.frame(as.matrix(beta)), data.frame(p_val),
+    SIMPLIFY = FALSE
   )
 }
--- tests/testthat/test-simulate.R
+++ tests/testthat/test-simulate.R
@@ -34,6 +34,15 @@ test_that("generate_network_normalizes", {
   expect_lte(max(abs(eigenvalues)), 1.0 + 1e-8)
 })
 
+test_that("generate_network_symmetry", {
+  D <- 10
+  p <- 0.5
+  R <- as.matrix(generate_network(D, p, symmetric = TRUE))
+  expect_true(all(R == t(R)))
+  R <- as.matrix(generate_network(D, p, symmetric = FALSE))
+  expect_false(any((R > 0) & (t(R) > 0)))
+})
+
 test_that("generate_dataset_runs", {
   N <- 10
   D <- 5
@@ -41,9 +50,23 @@ test_that("generate_dataset_runs", {
   p_beta <- 0.5
   p_network <- 0.5
   noise_ratio <- 0.5
-  result <- generate_dataset(N, M, D, p_beta, p_network, noise_ratio)
+  result <- generate_dataset(N, M, D, p_beta, p_network, noise_ratio, pleiotropy = TRUE)
   expect_equal(dim(result$Y), c(N, D))
   expect_equal(dim(result$X), c(N, M))
   expect_equal(dim(result$beta), c(M, D))
   expect_equal(dim(result$R), c(D, D))
+})
+
+test_that("generate_sumstats_works",{
+  N <- 10
+  D <- 2
+  M <- 2
+  p_beta <- 1.0
+  p_network <- 0.5
+  dataset <- generate_dataset(N, M, D, p_beta, p_network, pleiotropy = TRUE)
+  sumstats <- generate_sumstats(dataset$X, dataset$Y, normalize = FALSE)
+  expect_equal(sumstats$beta_hat[1,1], unname(lm(dataset$Y[,1] ~ dataset$X[,1])$coefficients)[2])
+  expect_equal(sumstats$beta_hat[1,2], unname(lm(dataset$Y[,2] ~ dataset$X[,1])$coefficients)[2])
+  expect_equal(sumstats$beta_hat[2,1], unname(lm(dataset$Y[,1] ~ dataset$X[,2])$coefficients)[2])
+  expect_equal(sumstats$beta_hat[2,2], unname(lm(dataset$Y[,2] ~ dataset$X[,2])$coefficients)[2])
 })
